121691f447b31b1b17c9679b679df7b718e3b70e
James Tiu <jamestiulawyer@gmail.com>
2021-04-15 23:40:20 -0600

    cleaned classes, changed key to string

--- Program.cs
+++ Program.cs
@@ -5,20 +5,14 @@ namespace Dictionary
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //string FirstName;
-            //string LastName;
-            //string EmailAddress;
-            //int RegistrationID;
-            //string NextCourtDate;
-
             CourthouseParticipant ctHouseParticipant1 = new CourthouseParticipant()
             {
                 FirstName = "Jessica",
                 LastName = "Jones",
                 EmailAddress = "Jess@JS.com",
-                RegistrationID = 345,
+                RegistrationID = "345",
                 NextCourtDate = "Apr25",
                 
             };
@@ -28,9 +22,9 @@ namespace Dictionary
                 FirstName = "Joe",
                 LastName = "Smith",
                 EmailAddress = "Joe@JS.com",
-                RegistrationID = 123,
+                RegistrationID = "123",
                 NextCourtDate = "Apr24",
-                
+
             };
 
             CourthouseParticipant ctHouseParticipant3 = new CourthouseParticipant()
@@ -38,30 +32,30 @@ namespace Dictionary
                 FirstName = "Jeff",
                 LastName = "Jones",
                 EmailAddress = "Jeff@JS.com",
-                RegistrationID = 234,
+                RegistrationID = "234",
                 NextCourtDate = "Apr22",
-                
+
             };
 
-            Dictionary<int, CourthouseParticipant> dictionaryCtHouseParticipants = new Dictionary<int, CourthouseParticipant>();
+            Dictionary<string, CourthouseParticipant> dictionaryCtHouseParticipants = new Dictionary<string, CourthouseParticipant>();
             dictionaryCtHouseParticipants.Add(ctHouseParticipant1.RegistrationID, ctHouseParticipant1);
-            dictionaryCtHouseParticipants.Add(ctHouseParticipant2.RegistrationID, ctHouseParticipant2);
-            dictionaryCtHouseParticipants.Add(ctHouseParticipant3.RegistrationID, ctHouseParticipant3);
+            //dictionaryCtHouseParticipants.Add(ctHouseParticipant2.RegistrationID, ctHouseParticipant2);
+            //dictionaryCtHouseParticipants.Add(ctHouseParticipant3.RegistrationID, ctHouseParticipant3);
 
-            CourthouseParticipant courthouseParticipant345 = dictionaryCtHouseParticipants[345];
+            CourthouseParticipant courthouseParticipant345 = dictionaryCtHouseParticipants["345"];
           
-            CourthouseParticipant courthouseParticipant123 = dictionaryCtHouseParticipants[123];
+            //CourthouseParticipant courthouseParticipant123 = dictionaryCtHouseParticipants[123];
          
-            CourthouseParticipant courthouseParticipant234 = dictionaryCtHouseParticipants[234];
+            //CourthouseParticipant courthouseParticipant234 = dictionaryCtHouseParticipants[234];
             Console.WriteLine("");
             Console.WriteLine("ID = {0}/ FirstName = {1}/ LastName = {2}/ NextCourtDate = {3}", courthouseParticipant345.RegistrationID, courthouseParticipant345.FirstName, courthouseParticipant345.LastName, courthouseParticipant345.NextCourtDate);
             Console.ReadLine();
-            Console.WriteLine("ID = {0}/ FirstName = {1}/ LastName = {2}/ NextCourtDate = {3}", courthouseParticipant123.RegistrationID, courthouseParticipant123.FirstName, courthouseParticipant123.LastName, courthouseParticipant123.NextCourtDate);
-            Console.ReadLine();
-            Console.WriteLine("ID = {0}/ FirstName = {1}/ LastName = {2}/ NextCourtDate = {3}", courthouseParticipant234.RegistrationID, courthouseParticipant234.FirstName, courthouseParticipant234.LastName, courthouseParticipant234.NextCourtDate);
+            //Console.WriteLine("ID = {0}/ FirstName = {1}/ LastName = {2}/ NextCourtDate = {3}", courthouseParticipant123.RegistrationID, courthouseParticipant123.FirstName, courthouseParticipant123.LastName, courthouseParticipant123.NextCourtDate);
+            //Console.ReadLine();
+            //Console.WriteLine("ID = {0}/ FirstName = {1}/ LastName = {2}/ NextCourtDate = {3}", courthouseParticipant234.RegistrationID, courthouseParticipant234.FirstName, courthouseParticipant234.LastName, courthouseParticipant234.NextCourtDate);
             Console.ReadLine();
 
-            foreach (KeyValuePair<int, CourthouseParticipant> participantKeyValuePair in dictionaryCtHouseParticipants)
+            foreach (KeyValuePair<string, CourthouseParticipant> participantKeyValuePair in dictionaryCtHouseParticipants)
             {
                 Console.WriteLine("ID = {0}", participantKeyValuePair.Key);
                 CourthouseParticipant ctHousePart = participantKeyValuePair.Value;
@@ -73,13 +67,6 @@ namespace Dictionary
 
         }
 
-        public class CourthouseParticipant
-        {
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string EmailAddress { get; set; }
-            public int RegistrationID { get; set; }
-            public string NextCourtDate { get; set; }
-        }
+       
     }
 }
